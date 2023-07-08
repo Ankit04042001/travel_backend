27d193cb3c2f0860d2c8b80510425fcb146273f2
aman <ak4440204@gmail.com>
2023-07-08 17:56:32 +0530

    Changing database name

--- backend/settings.py
+++ backend/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'travel-backend',
+        'NAME': 'travel',
         'USER': 'travelBackendUser',
         'PASSWORD': 'Sunny@2000',
         'HOST': 'localhost',
