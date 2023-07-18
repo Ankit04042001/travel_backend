0a8692043e37ec1a3a21949dfbd0f9785efed837
aman <ak4440204@gmail.com>
2023-07-18 21:05:36 +0530

    allowing cors for domain

--- backend/settings.py
+++ backend/settings.py
@@ -139,7 +139,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:3000',
-    'http://127.0.0.1:5501'
+    'http://127.0.0.1:5501',
+    'https://travel.kuchhbhi.in',
+    'https://www.travel.kuchhbhi.in'
 ]
 
 AUTH_USER_MODEL = 'base.User'
